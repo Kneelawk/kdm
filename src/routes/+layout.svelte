bfa41ef8781ae8356ef8d4a2ccf6013cff004787
Kneelawk <kneelawk@gmail.com>
2023-08-20 04:20:58 -0700

    Mess around with background colors

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -2,4 +2,7 @@
     import '../app.css'
 </script>
 
-<slot/>
+<div class="flex flex-col h-full">
+    <h1 class="text-2xl text-white bg-gray-900 p-4">Kneelawk's Download Manager</h1>
+    <slot/>
+</div>
--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,2 +1,3 @@
-<h1 class="text-3xl font-bold">Welcome to SvelteKit</h1>
-<p>Visit <a class="underline text-blue-500" href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation</p>
+<div class="h-full bg-gray-700 overflow-scroll">
+
+</div>
